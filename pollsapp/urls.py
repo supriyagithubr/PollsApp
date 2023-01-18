d13f7d3895e4bc9c74eae3d1b9439e7c83f3c647
supriyakale12345 <supriya.k.kale@gmail.com>
2023-01-18 12:25:06 +0530

    First Commit

---
+++ pollsapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name= 'index'),
+    path('vote/<str:pk>', views.vote, name= 'vote'),
+    path('result/<str:pk>', views.result, name= 'result'),
+    
+]
